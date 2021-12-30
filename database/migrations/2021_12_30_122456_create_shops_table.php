61ae867b8105e4085a792d5ac7d7f1521f63aac3
ayano minami <ayanodame.surume@gmail.com>
2021-12-30 21:46:56 +0900

    create shops migration

---
+++ database/migrations/2021_12_30_122456_create_shops_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',50);
+            $table->unsignedBigInteger('area_id');
+            $table->unsignedBigInteger('genre_id');
+            $table->string('summary',500);
+            $table->time('open_time');
+            $table->time('close_time');
+            $table->string('image_url',100);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCuurent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
