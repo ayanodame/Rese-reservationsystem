4c380bd9df0e962be7ec0b9b538ab0dd518cc33d
ayano minami <ayanodame.surume@gmail.com>
2021-12-31 00:24:36 +0900

    create Shop Model

---
+++ app/Models/Shop.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'area_id',
+        'genre_id',
+        'summary',
+        'open_time',
+        'close_time',
+        'image_url',
+    ];
+}
