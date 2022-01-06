d76708644747a9c51f1ec93167bcccf886a869ec
ayano minami <ayanodame.surume@gmail.com>
2022-01-06 18:25:33 +0900

    検索用のプルダウンを追加（Area、Genreモデルの連携とviewでgenreとareaのforeach文の追加）

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Shop;
+use App\Models\Area;
+use App\Models\Genre;
 
 class ShopController extends Controller
 {
     public function index() {
         $items=Shop::all();
-        return view('shopdetail',['items'=>$items]);
+        $genres=Genre::all();
+        $areas=Area::all();
+        return view('shopdetail',['items'=>$items,'genres'=>$genres,'areas'=>$areas]);
     }
 }
--- resources/views/shopdetail.blade.php
+++ resources/views/shopdetail.blade.php
@@ -1,4 +1,20 @@
 <!-- 飲食店詳細ページ用 -->
+<form action="/search" method="post">
+@csrf
+<select name="area">
+<option value="">All area</option>
+@foreach($areas as $area)
+<option value="{{$area->name}}">{{$area->name}}</option>
+@endforeach
+</select>
+<select name="genre">
+<option value="">All genre</option>
+@foreach($genres as $genre)
+<option value="{{$genre->name}}">{{$genre->name}}</option>
+@endforeach
+</select>
+<input type="text" name="">
+</form>
 <p>飲食店詳細ページ</p>
 @foreach($items as $item)
 <p>{{$item->name}}</p>
