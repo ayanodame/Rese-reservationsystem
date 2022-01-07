04f9cc4a95b55ae619b47b43d2febf803e5dddb6
ayano minami <ayanodame.surume@gmail.com>
2022-01-08 00:32:28 +0900

    整理をして、indexメソッドに集約。（Shop::all()は不要ということがわかったので、削除）・（results=>$resultsの場所を下に移動させました）

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,25 +9,19 @@ use App\Models\Genre;
 
 class ShopController extends Controller
 {
-    public function index() {
-        $items=Shop::all();
-        $genres=Genre::all();
-        $areas=Area::all();
-        $keywords=[
-            'name'=>'',
-        ];
-        return view('shopdetail',['items'=>$items,'genres'=>$genres,'areas'=>$areas],$keywords);
-    }
-    public function search(Request $request){
+    public function index(Request $request){
         $results=Shop::where('name','LIKE',"%{$request->input_name}%")->where('area_id','LIKE',"%{$request->input_area}")->where('genre_id','LIKE',"%{$request->input_genre}")->get();
         $param=[
             'name'=>$request->input_name,
-            'results'=>$results,
             'area_id'=>$request->input_area,
             'genre_id'=>$request->input_genre,
+            'results'=>$results,
         ];
         $areas=Area::all();
         $genres=Genre::all();
-        return view('shopdetail',['areas'=>$areas,'genres'=>$genres],$param);
+        $keywords=[
+            'name'=>'',
+        ];
+        return view('shopdetail',['areas'=>$areas,'genres'=>$genres],$param,$keywords);
     }
 }
