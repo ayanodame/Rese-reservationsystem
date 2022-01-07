9b454b0264d41c3f771f6c8201ff8406c22c039b
ayano minami <ayanodame.surume@gmail.com>
2022-01-07 17:05:16 +0900

    検索searchメソッドの追加（後で合体予定・・・）、Areaと店名検索のコード完成

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -13,6 +13,19 @@ class ShopController extends Controller
         $items=Shop::all();
         $genres=Genre::all();
         $areas=Area::all();
-        return view('shopdetail',['items'=>$items,'genres'=>$genres,'areas'=>$areas]);
+        $keywords=[
+            'name'=>'',
+        ];
+        return view('shopdetail',['items'=>$items,'genres'=>$genres,'areas'=>$areas],$keywords);
+    }
+    public function search(Request $request){
+        $results=Shop::where('name','LIKE',"%{$request->input_name}%")->where('area_id','LIKE',"%{$request->input_area}")->get();
+        $param=[
+            'name'=>$request->input_name,
+            'results'=>$results,
+            'area_id'=>$request->input_area,
+        ];
+        $areas=Area::all();
+        return view('shopdetail',['areas'=>$areas],$param);
     }
 }
