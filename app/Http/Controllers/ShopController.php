4f56f5b5bb456478eb42dd71dc0b02a5aec8b5dd
ayano minami <ayanodame.surume@gmail.com>
2022-01-19 23:45:42 +0900

    $param（連想配列）にまとめました。

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -10,12 +10,14 @@ use App\Models\Genre;
 class ShopController extends Controller
 {
     public function index(Request $request){
-        $keywords=$request->input_keyword;
-        $area=$request->input_area;
-        $genre=$request->input_genre;
+        $param=[
+            $keywords=$request->input_keyword,
+            $area=$request->input_area,
+            $genre=$request->input_genre,
+        ];
         $shops=Shop::searchShop($area,$genre,$keywords);
         $areas=Area::all();
         $genres=Genre::all();
-        return view('shoplist',['shops'=>$shops,'areas'=>$areas,'genres'=>$genres]);
+        return view('shoplist',['shops'=>$shops,'areas'=>$areas,'genres'=>$genres],$param);
     }
 }
