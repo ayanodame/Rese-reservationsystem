f3373df842a248ef6a3a731a951e790c386042b9
ayano minami <ayanodame.surume@gmail.com>
2022-01-25 18:16:32 +0900

    会員登録機能・cssの追加

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,24 +9,55 @@
 
 @section('body')
 
-<p>会員登録ページ</p>
+<header class="system-header">
+  <div class="header-top">
+    <div class="menu" id="menu">
+      <span class="menu__line--top"></span>
+      <span class="menu__line--middle"></span>
+      <span class="menu__line--bottom"></span>
+    </div>
+    <div class="menu-title">
+      <a href="/" class="menu-title__name">Rese</a>
+    </div>
+  </div>
+</header>
 
-@if($errors->has('name'))
-<p class="error-message">{{$errors->first('name')}}</p>
-@endif
-@if($errors->has('email'))
-<p class="error-message">{{$errors->first('email')}}</p>
-@endif
-@if($errors->has('password'))
-<p class="error-message">{{$errors->first('password')}}</p>
-@endif
-
-<form action="/register" method="post">
-  @csrf
-  <input type="text" name="name" placeholder="Username" value="{{ old('name') }}">
-  <input type="email" name="email" placeholder="Email" value="{{ old('email') }}">
-  <input type="text" name="password" placeholder="Password" value="{{ old('password') }}">
-  <input type="submit" value="登録">
-</form>
-
+<main class="register">
+  <div class="box">
+    <div class="box__title">
+      <p class="box__title__name">Registration</p>
+    </div>
+    <div class="box__content">
+      <form action="/register" method="post">
+        @csrf
+        <div class="box__content__form">
+          @if($errors->has('name'))
+          <p class="error-message">{{$errors->first('name')}}</p>
+          @endif
+          <input type="text" class="form__name" name="name" placeholder="Username" value="{{ old('name') }}">
+        </div>
+        <div class="box__content__form">
+          @if($errors->has('email'))
+          <p class="error-message">{{$errors->first('email')}}</p>
+          @endif
+          <div class="form__email">
+            <input type="text" class="form__email" name="email" placeholder="Email" value="{{ old('email') }}">
+          </div>
+        </div>
+        <div class="box__content__form">
+          @if($errors->has('password'))
+          <p class="error-message">{{$errors->first('password')}}</p>
+          @endif
+          <div class="form__password">
+            <input type="text" name="password" placeholder="Password" value="{{ old('password') }}">
+          </div>
+        </div>
+        <div class="box__content__button">
+          <input type="submit" value="登録">
+        </div>
+      </form>
+    </div>
+  </div>
+</main>
+<script src="{{ asset('js/main.js') }}"></script>
 @endsection
