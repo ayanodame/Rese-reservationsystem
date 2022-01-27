39127a9fa6713d74f1b97cfd74e8f175686ee0e9
ayano minami <ayanodame.surume@gmail.com>
2022-01-27 13:43:22 +0900

    エラーメッセージによるデザイン崩れを修正

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -31,25 +31,37 @@
       <form action="/register" method="post">
         @csrf
         <div class="box__content__form">
-          @if($errors->has('name'))
-          <p class="error-message">{{$errors->first('name')}}</p>
-          @endif
-          <img class="form__image" src="icon/人物アイコン.svg" alt="人物アイコン">
-          <input type="text" class="form__name" name="name" placeholder="Username" value="{{ old('name') }}">
+          <div class="form__error">
+            @if($errors->has('name'))
+            <p class="error-message">{{$errors->first('name')}}</p>
+            @endif
+          </div>
+          <div class="form__text">
+            <img class="form__text__image" src="icon/人物アイコン.svg" alt="人物アイコン">
+            <input type="text" class="form__text__name" name="name" placeholder="Username" value="{{ old('name') }}">
+          </div>
         </div>
         <div class="box__content__form">
-          @if($errors->has('email'))
-          <p class="error-message">{{$errors->first('email')}}</p>
-          @endif
-          <img class="form__image" src="icon/メールの無料アイコン.svg" alt="メールアイコン">
-          <input type="text" class="form__email" name="email" placeholder="Email" value="{{ old('email') }}">
+          <div class="form__error">
+            @if($errors->has('email'))
+            <p class="error-message">{{$errors->first('email')}}</p>
+            @endif
+          </div>
+          <div class="form__text">
+            <img class="form__text__image" src="icon/メールの無料アイコン.svg" alt="メールアイコン">
+            <input type="text" class="form__text__email" name="email" placeholder="Email" value="{{ old('email') }}">
+          </div>
         </div>
         <div class="box__content__form">
-          @if($errors->has('password'))
-          <p class="error-message">{{$errors->first('password')}}</p>
-          @endif
-          <img class="form__image" src="icon/カギアイコン.svg" alt="カギアイコン">
-          <input type="text" class="form__password" name="password" placeholder="Password" value="{{ old('password') }}">
+          <div class="form__error">
+            @if($errors->has('password'))
+            <p class="error-message">{{$errors->first('password')}}</p>
+            @endif
+          </div>
+          <div class="form__text">
+            <img class="form__text__image" src="icon/カギアイコン.svg" alt="カギアイコン">
+            <input type="text" class="form__text__password" name="password" placeholder="Password" value="{{ old('password') }}">
+          </div>
         </div>
         <div class="box__content__button">
           <input type="submit" class="form__button" value="登録">
