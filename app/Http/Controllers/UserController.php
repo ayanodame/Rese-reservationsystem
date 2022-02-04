18945ebe020409ea35ae8e00f7a72bdaa593fe8d
ayano minami <ayanodame.surume@gmail.com>
2022-02-05 00:57:09 +0900

    会員登録機能、passwordをHash化にしました。（でないとログインができない為）

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -16,8 +17,11 @@ class UserController extends Controller
 
     public function register(RegisterRequest $request)
     {
-        $form = $request->all();
-        User::create($form);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
         return view('thanks');
     }
 
