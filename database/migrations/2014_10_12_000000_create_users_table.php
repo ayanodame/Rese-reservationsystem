3f93f99073d5c0b407c5ecdfc4f4226792227514
ayano minami <ayanodame.surume@gmail.com>
2022-02-04 17:27:53 +0900

    一旦uniqueキーありでherokuデプロイしてみるため、uniqueキーを追加

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('email',50);
+            $table->string('email',50)->unique();
             $table->string('password',50);
             $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
