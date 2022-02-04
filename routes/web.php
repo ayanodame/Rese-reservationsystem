dab72f965ec27696a2ce61aa94b50c47acae0760
ayano minami <ayanodame.surume@gmail.com>
2022-02-05 00:48:55 +0900

    AuthControllerの追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 /*
 |--------------------------------------------------------------------------
