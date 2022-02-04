eb4cb2ab009ccd37a05a181b9e26307d888e41b6
ayano minami <ayanodame.surume@gmail.com>
2022-02-04 17:24:19 +0900

    herokuデプロイするために修正しました。

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('email',255)->unique();
+            $table->string('email',50);
             $table->string('password',50);
             $table->rememberToken();
             $table->timestamp('created_at')->useCurrent();
