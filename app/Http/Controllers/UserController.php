08118e5c0e60a4fe3158888fa02b9d3b3916fd4a
ayano minami <ayanodame.surume@gmail.com>
2022-02-04 17:22:43 +0900

    ログイン機能をとりあえず、教材通りに作成してみました。

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,31 +9,42 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function registerView(Request $request){
+    public function registerView(Request $request)
+    {
         return view('register');
     }
 
-    public function register(RegisterRequest $request){
-        $form=$request->all();
+    public function register(RegisterRequest $request)
+    {
+        $form = $request->all();
         User::create($form);
         return view('thanks');
     }
-    public function mypageView() {
-        $user=Auth::user();
-        return view('mypage',['user'=>$user]);
+
+    public function mypageView()
+    {
+        $user = Auth::user();
+        return view('mypage', ['user' => $user]);
     }
-    public function loginView(Request $request) {
-        $text=['text' =>'ログインしてください。'];
-        return view('login',$text);
+
+    public function loginView(Request $request)
+    {
+        $text = ['text' => 'ログインして下さい。'];
+        return view('login', $text);
     }
-    public function login(Request $request) {
-        $email=$request->email;
-        $password=$request->password;
-        if(Auth::attempt(['email'=>$email,
-        'password'=>$password])) {
-            return view('mypage');
+
+    public function login(Request $request)
+    {
+        $email = $request->email;
+        $password = $request->password;
+        if (Auth::attempt([
+            'email' => $email,
+            'password' => $password
+        ])) {
+            $text =   Auth::user()->name . 'さんがログインしました';
+        } else {
+            $text = 'ログインに失敗しました';
         }
-        $text="ログインに失敗しました。";
-        return view('login', ['text'=>$text]);
+        return view('login', ['text' => $text]);
     }
 }
