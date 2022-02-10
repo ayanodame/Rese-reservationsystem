cb2a3aa516bd4328f700bc438ca3e854df0c252c
ayano minami <ayanodame.surume@gmail.com>
2022-02-11 01:11:19 +0900

    店舗詳細情報のコードを追加

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,13 +9,22 @@ use App\Models\Genre;
 
 class ShopController extends Controller
 {
-    public function index(Request $request){
-        $keywords=$request->input_keyword;
-        $areaId=$request->input_area;
-        $genreId=$request->input_genre;
-        $shops=Shop::searchShop($areaId,$genreId,$keywords);
-        $areas=Area::all();
-        $genres=Genre::all();
-        return view('shoplist',['shops'=>$shops,'areas'=>$areas,'genres'=>$genres,'keywords'=>$keywords,'areaId'=>$areaId,'genreId'=>$genreId]);
+    public function index(Request $request)
+    {
+        $keywords = $request->input_keyword;
+        $areaId = $request->input_area;
+        $genreId = $request->input_genre;
+        $shops = Shop::searchShop($areaId, $genreId, $keywords);
+        $areas = Area::all();
+        $genres = Genre::all();
+        return view('index', ['shops' => $shops, 'areas' => $areas, 'genres' => $genres, 'keywords' => $keywords, 'areaId' => $areaId, 'genreId' => $genreId]);
+    }
+
+    public function detaillView(Shop $shop)
+    {
+        $shopData = [
+            'item' => $shop,
+        ];
+        return view('shopdetail', $shopData);
     }
 }
