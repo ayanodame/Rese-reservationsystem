db664e7e626b63cee45c42232aa20ecb1e200d88
ayano minami <ayanodame.surume@gmail.com>
2022-02-11 01:14:09 +0900

    店舗詳細情報のviewを追加（ちゃんと反映されるかを確認したかったため、一旦簡単なコードにしてます）

--- resources/views/shopdetail.blade.php
+++ resources/views/shopdetail.blade.php
@@ -1,1 +1,18 @@
 <!--飲食店詳細 -->
+@extends('layouts.default')
+@section('title','飲食店一覧')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/shopdetail.css') }}">
+@endsection
+
+
+@section('main')
+<img src="{{$item->image_url}}" alt="{{$item->name}}">
+<p>{{$item->name}}</p>
+<p>{{$item->area->name}}</p>
+<p>{{$item->genre->name}}</p>
+<p>{{$item->summary}}</p>
+</section>
+</main>
+@endsection
