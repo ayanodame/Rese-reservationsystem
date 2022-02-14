834d0a253abed2268b71fcb3eb2b4356f528c7d1
ayano minami <ayanodame.surume@gmail.com>
2022-02-14 22:05:45 +0900

    予約登録時で使用するモデルの記述

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Shop;
+use App\Models\Reservation;
 
 class ReservationController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        //
+    }
 }
