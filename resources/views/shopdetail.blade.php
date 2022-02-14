9ae2f414f0512c210f7bbb64062e6c29f8464016
ayano minami <ayanodame.surume@gmail.com>
2022-02-14 22:04:39 +0900

    変数名を$itemから$itemsに変更。また、予約登録人数にfor文を追加

--- resources/views/shopdetail.blade.php
+++ resources/views/shopdetail.blade.php
@@ -8,11 +8,29 @@
 
 
 @section('main')
-<img src="{{$item->image_url}}" alt="{{$item->name}}">
-<p>{{$item->name}}</p>
-<p>{{$item->area->name}}</p>
-<p>{{$item->genre->name}}</p>
-<p>{{$item->summary}}</p>
-</section>
+<main>
+  <section>
+    <img src="{{$items->image_url}}" alt="{{$items->name}}">
+    <p>{{$items->name}}</p>
+    <p>{{$items->area->name}}</p>
+    <p>{{$items->genre->name}}</p>
+    <p>{{$items->summary}}</p>
+  </section>
+  <aside>
+    <p>予約</p>
+    <form action="/reserve" method="post">
+      @csrf
+      <input type="date" name="date">
+      <select name="input_time" value="">
+        <option value="input_time">{{$items->open_time}}</option>
+      </select>
+      <select name="input_people">
+        @for($people=1; $people<=10; $people++) <option value="{{$people}}">{{$people}}人</option>
+          @endfor
+      </select>
+      <input type="submit" value="予約する">
+    </form>
+
+  </aside>
 </main>
 @endsection
