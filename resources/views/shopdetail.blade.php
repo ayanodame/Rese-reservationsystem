26cb6364775ae1c67b3e2a9f63231145adf75be8
ayano minami <ayanodame.surume@gmail.com>
2022-02-15 18:23:27 +0900

    inputタグの追加（店舗名）

--- resources/views/shopdetail.blade.php
+++ resources/views/shopdetail.blade.php
@@ -20,7 +20,10 @@
     <p>予約</p>
     <form action="/reserve" method="post">
       @csrf
+      <input type="hidden" name="name" value="{{$items->name}}">
       <input type="date" name="date">
+      <!--時間の繰り返し文がわからない・・・ 30分ごと追加 optionにかませるtime型リストを作る-->
+
       <select name="input_time" value="">
         <option value="input_time">{{$items->open_time}}</option>
       </select>
@@ -28,6 +31,7 @@
         @for($people=1; $people<=10; $people++) <option value="{{$people}}">{{$people}}人</option>
           @endfor
       </select>
+      <p>{{$items->name}}</p>
       <input type="submit" value="予約する">
     </form>
 
