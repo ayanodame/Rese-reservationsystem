f817b75795fb4295e4d346950fd35408e45c2e3e
ayano minami <ayanodame.surume@gmail.com>
2022-02-18 00:41:32 +0900

    予約のinputタグを選択したら、下に反映されるJavaScriptを追加

--- public/js/main.js
+++ public/js/main.js
@@ -2,3 +2,24 @@ const target = document.getElementById("menu");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
 });
+
+window.onload = function () {
+    getValue();
+    var $formObject = document.getElementById( "reserveForm" );
+    for( var $i = 0; $i < $formObject.length; $i++ ) {
+        $formObject.elements[$i].onkeyup = function(){
+            getValue();
+        };
+        $formObject.elements[$i].onchange = function(){
+            getValue();
+        };
+    }
+    document.getElementById( "reserveCheckLength" ).innerHTML = $formObject.length;
+}
+
+function getValue() {
+    var $formObject = document.getElementById( "reserveForm" );
+    document.getElementById( "shopDate" ).innerHTML = $formObject.date.value;
+    document.getElementById("shopTime").innerHTML = $formObject.time.value;
+    document.getElementById( "shopNumber" ).innerHTML = $formObject.number.value;
+}
