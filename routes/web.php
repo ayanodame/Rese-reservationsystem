1a5bd2768ed1d2af0de0f910f7e93a05fb488692
ayano minami <ayanodame.surume@gmail.com>
2022-02-21 18:04:15 +0900

    特に変更なし

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,5 @@ Route::post('/register', [UserController::class, 'register']);
 Route::get('/mypage', [UserController::class, 'mypageView'])->middleware('auth');
 Route::get('/login', [UserController::class, 'loginView'])->name('login');
 Route::post('/login', [UserController::class, 'login']);
-Route::get('/detail/{shop}',[ShopController::class,'detaillView']);
-Route::post('/reserve',[ReservationController::class,'register']);
+Route::get('/detail/{shop}', [ShopController::class, 'detaillView']);
+Route::post('/reserve', [ReservationController::class, 'register']);
