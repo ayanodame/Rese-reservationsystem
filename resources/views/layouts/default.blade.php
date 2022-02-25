6f7e3fcc1e032f371c50b11d87bd49d3fa142834
ayano minami <ayanodame.surume@gmail.com>
2022-02-25 23:32:46 +0900

    ドロワーメニューの追加

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -29,6 +29,21 @@
         <a href="/" class="menu-title__name">Rese</a>
       </div>
     </div>
+    <div class="header-menu">
+      <nav class="drwermenu" id="nav">
+        <ul class="drwermenu__content">
+          @if (Auth::check())
+          <li class="user-menu"><a href="/" class="menu__link">Home</a></li>
+          <li class="user-menu"><a href="まだ作成してない" class="menu__link">Logout</a></li>
+          <li class="user-menu"><a href="/mypage" class="menu__link">Mypage</a></li>
+          @else
+          <li class="guest-menu"><a href="/" class="menu__link">Home</a></li>
+          <li class="guest-menu"><a href="/register" class="menu__link">Registration</a></li>
+          <li class="guest-menu"><a href="/login" class="menu__link">Login</a></li>
+          @endif
+        </ul>
+      </nav>
+    </div>
     @yield('side')
   </header>
 
