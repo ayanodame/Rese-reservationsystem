36a75b04d7a2f478c384154f427cf35e23f3602c
ayano minami <ayanodame.surume@gmail.com>
2022-02-25 15:47:55 +0900

    getValueを削除し、それぞれfunctionをあてるように変更しました。

--- public/js/check.js
+++ public/js/check.js
@@ -1,20 +1,16 @@
 window.onload = function () {
     document.getElementById( "inputDate" ).onchange = function(){
-        getValue();
-    };
-    document.getElementById("inputTime").onchange = function () {
-        getValue();
-    };
-    document.getElementById("inputNumber").onchange = function () {
-        getValue();
-    }
-}
-
-function getValue() {
-    var $dategetValue = document.getElementById("inputDate").value;
+        var $dategetValue = document.getElementById("inputDate").value;
     document.getElementById("outputDate").innerHTML = $dategetValue;
-    var $timegetValue = document.getElementById( "inputTime" ).value;
+    };
+
+    document.getElementById("inputTime").onchange = function () {
+        var $timegetValue = document.getElementById( "inputTime" ).value;
     document.getElementById("outputTime").innerHTML = $timegetValue;
-    var $numbergetValue = document.getElementById("inputNumber").value;
+    };
+
+    document.getElementById("inputNumber").onchange = function () {
+        var $numbergetValue = document.getElementById("inputNumber").value;
     document.getElementById("outputNumber").innerHTML = $numbergetValue;
+    }
 }
