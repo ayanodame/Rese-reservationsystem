a714df92c586cab32462a2e2965d1ad680aa4d8a
ayano minami <ayanodame.surume@gmail.com>
2022-03-02 17:43:31 +0900

    マイページに表示する予約情報のコードを追加しました。

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -2,14 +2,19 @@
 @section('title','Rese ログイン')
 
 @section('css')
-  <link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
+<link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
 @endsection
 
 
 @section('main')
 
 @if (Auth::check())
-<p>{{$user->name}}さん、こんにちは</p>
+<p>{{$items->user->name}}さん</p>
+<p>{{$items->shop->name}}</p>
+<p>{{$items->use_date}}</p>
+<p>{{$items->use_time}}</p>
+<p>{{$items->people}}人</p>
+
 
 @else
 <p>ログインしてください</p>
