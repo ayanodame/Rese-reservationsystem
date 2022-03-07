4402634deb8b5e64a2c2da9775dc2dfddcb3aa62
ayano minami <ayanodame.surume@gmail.com>
2022-03-07 16:16:02 +0900

    mypageに表示するいいね店舗の表示コードを記載

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
 use App\Models\Reservation;
 use App\Models\Shop;
+use App\Models\Like;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -32,8 +33,10 @@ class UserController extends Controller
     {
         $user = Auth::user();
         $reservations = Reservation::with('user')->get();
+        $likes = Like::where('user_id', Auth::id())->get();
         $data = [
             'reservations' => $reservations,
+            'likes' => $likes,
             'user' => $user,
         ];
         return view('mypage', $data);
