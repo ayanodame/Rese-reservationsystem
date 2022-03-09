258e33d4090b345f4b3c9ac866faa7eab312d9f0
ayano minami <ayanodame.surume@gmail.com>
2022-03-09 17:29:21 +0900

    likeメソッドに記載していた、Auth::user()を削除。

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -10,7 +10,6 @@ class LikeController extends Controller
 {
     public function like(Request $request)
     {
-        $user = Auth::user();
         $like = Like::create([
             'shop_id' => $request->shop_id,
             'user_id' => Auth::id(),
@@ -20,7 +19,7 @@ class LikeController extends Controller
     }
     public function unlike(Request $request)
     {
-        $like=Like::where('shop_id',$request->shop_id)->where('user_id',Auth::id())->first();
+        $like = Like::where('shop_id', $request->shop_id)->where('user_id', Auth::id())->first();
         $like->delete();
         return redirect()->back();
     }
