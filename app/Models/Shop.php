e0f653fc268cf86e7c5ab0485bb36843b132a63f
ayano minami <ayanodame.surume@gmail.com>
2022-03-09 17:30:51 +0900

    配列を組むコードを削除。そのままID判定できるコードに修正。また、ID判定のメソッド名の変更(is_liked_by_auth_user()→LikedByAuthUser())

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -55,18 +55,15 @@ class Shop extends Model
         $items = $query->orderBy('area_id')->orderBy('genre_id')->get();
         return $items;
     }
-    public function is_liked_by_auth_user()
+    public function LikedByAuthUser()
     {
         $id = Auth::id();
-        $likers = array();
         foreach ($this->likes as $like) {
-            array_push($likers, $like->user_id);
-        }
-
-        if (in_array($id, $likers)) {
-            return true;
-        } else {
-            return false;
+            if ($id = $like->user_id) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
