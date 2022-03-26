469af83b6aab43b15305f33198479c9138729c8b
ayano minami <ayanodame.surume@gmail.com>
2022-03-26 22:44:37 +0900

    認証機能用のルートを追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ReservationController;
@@ -19,12 +21,13 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 |
 */
 
-Route::get('/', [ShopController::class, 'index']);
+Route::get('/', [ShopController::class, 'index'])->name('verification.notice');
 Route::get('/like', [LikeController::class, 'like']);
 Route::get('/unlike', [LikeController::class, 'unlike']);
 Route::get('/register', [UserController::class, 'registerView'])->middleware('guest');
 Route::post('/register', [UserController::class, 'register']);
-Route::get('/mypage', [UserController::class, 'mypageView'])->middleware('auth');
+Route::get('/thanks',[UserController::class,'thanksView'])->name('verification.verify');
+Route::get('/mypage', [UserController::class, 'mypageView'])->middleware('verified');
 Route::get('/login', [UserController::class, 'loginView'])->middleware('guest')->name('login');
 Route::post('/login', [UserController::class, 'login']);
 Route::get('/detail/{shop}', [ShopController::class, 'detaillView']);
@@ -33,3 +36,26 @@ Route::get('/logout', [AuthenticatedSessionController::class, 'destroy']);
 Route::get('delete', [ReservationController::class, 'delete']);
 Route::get('/update/{reservation}', [ReservationController::class, 'updateView']);
 Route::post('/update', [ReservationController::class, 'update']);
+
+
+//未承認の方がアクセスしようとした時に表示されるルート
+Route::get('/email/verify', function () {
+  return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+
+//メールに記載されている本登録ボタンをクリックした時に生成されるリクエストを処理するルート
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+  $request->fulfill();
+
+  return view('thanks');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+
+
+//承認未実施の時のルート及びメールの再送信
+Route::post('/email/verification-notification', function (Request $request) {
+  $request->user()->sendEmailVerificationNotification();
+
+  return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
