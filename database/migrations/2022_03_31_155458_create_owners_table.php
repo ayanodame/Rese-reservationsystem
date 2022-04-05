bbe879955d9ae8ac3ad2d8d4be1d751e6d6e69ce
ayano minami <ayanodame.surume@gmail.com>
2022-04-05 13:32:46 +0900

    Owners tableを追加の為、Seeder・migrationをそれぞれ追加しました。

---
+++ database/migrations/2022_03_31_155458_create_owners_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOwnersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owners', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 50)->unique();
+            $table->string('password', 100);
+            $table->unsignedBigInteger('shop_id')->unique();
+            $table->unsignedBigInteger('reservation_id')->nullable();
+            $table->rememberToken();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owners');
+    }
+}
