297c4bed6f2310fde862b48fb25e5ba5d5d166cf
ayano minami <ayanodame.surume@gmail.com>
2022-04-08 18:46:25 +0900

    管理画面で検索機能・エリア登録機能・ジャンル登録機能を追加

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Owner;
+use App\Models\Shop;
 use App\Models\Area;
+use App\Models\Genre;
+use App\Models\Owner;
 
 class AdminController extends Controller
 {
     public function adminView(Request $request)
     {
-        $owners = Owner::Paginate(5);
-        return view('admin', ['owners' => $owners]);
+        $keywords = $request->input_keyword;
+        $shopId = $request->input_shop;
+        $email = $request->input_email;
+        $items = Owner::searchOwner($shopId, $email, $keywords);
+        $shops = shop::all();
+        $areas = Area::all();
+        $genres = Genre::all();
+        return view('admin', ['keywords' => $keywords, 'shopId' => $shopId, 'email' => $email, 'keywords' => $keywords, 'items' => $items, 'shops' => $shops, 'areas' => $areas, 'genres' => $genres]);
     }
 
     public function areaView()
@@ -25,6 +33,20 @@ class AdminController extends Controller
             'name' => $request->name,
         ]);
         unset($area['_token']);
+        return redirect('/admin');
+    }
+
+    public function genreView()
+    {
+        return view('genre_register');
+    }
+
+    public function genreRegister(Request $request)
+    {
+        $area = Genre::create([
+            'name' => $request->name,
+        ]);
+        unset($genre['_token']);
         return redirect('/admin');
     }
 }
