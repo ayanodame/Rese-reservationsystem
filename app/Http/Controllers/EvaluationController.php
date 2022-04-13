97f482ad0f61774e16ecc27c51480d28c6e330ba
ayano minami <ayanodame.surume@gmail.com>
2022-04-13 17:37:18 +0900

    評価登録機能の追加

--- app/Http/Controllers/EvaluationController.php
+++ app/Http/Controllers/EvaluationController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Evaluation;
+use App\Http\Requests\EvaluationRequest;
 use App\Models\Reservation;
 
 class EvaluationController extends Controller
@@ -11,11 +12,18 @@ class EvaluationController extends Controller
     public function registerView(Reservation $reservation)
     {
         $reservationData = [
-            'items' => $reservation,
+            'item' => $reservation,
         ];
         return view('user.evaluation_register', $reservationData);
     }
     public function register(EvaluationRequest $request){
-        //
+        $evaluation = Evaluation::create([
+            'user_id' => $request->user_id,
+            'shop_id' => $request->shop_id,
+            'rate' => $request->rate,
+            'comment' =>$request->comment,
+        ]);
+        unset($evaluation['_token']);
+        return view('user.evaluation_registered');
     }
 }
